71c8bc60423c82128fd9bd2afca2183e8c4a5657
Tayson Butler <taycoolbroom@gmail.com>
2021-07-20 15:32:03 +0800

    init proj again

---
+++ pages/rooms_index/rooms_index.js
@@ -0,0 +1,95 @@
+// pages/rooms_index/rooms_index.js
+const app = getApp();
+Page({
+
+  /**
+   * Page initial data
+   */
+   data: {
+    rooms: [],
+  },
+  clickMe: function () {
+    this.setData({ text: "See Venue Details" })
+  },
+  switchToRoom: function() {
+    wx.switchTab({
+      url: '/pages/room_show/room_show',
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {},
+    })
+  },
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+    const page = this;
+    console.log("page loaded")
+    wx.request({
+      url: 'http://localhost:3000/api/v1/rooms',
+      method: 'GET',
+      success(res) {
+        console.log(res)
+        const rooms = res.data;
+        console.log("rooms:", rooms)
+        page.setData({
+          rooms: rooms
+        });
+        wx.hideToast();
+      }
+    });
+  },
+
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+    this.setData({
+      rooms: app.globalData.rooms
+    })
+  },
+
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
