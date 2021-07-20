343bd5a835ca3f4a6bce980e4b62228f52558ae5
Alex Aguilar <aaguilar.0101@gmail.com>
2021-07-20 18:37:14 +0800

    added navigation to room show page

--- pages/rooms_index/rooms_index.js
+++ pages/rooms_index/rooms_index.js
@@ -8,17 +8,17 @@ Page({
    data: {
     rooms: [],
   },
-  clickMe: function () {
-    this.setData({ text: "See Venue Details" })
-  },
-  switchToRoom: function() {
-    wx.switchTab({
-      url: '/pages/room_show/room_show',
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
-    })
-  },
+  // clickMe: function () {
+  //   this.setData({ text: "See Venue Details" })
+  // },
+  // switchToRoom: function() {
+  //   wx.switchTab({
+  //     url: '/pages/room_show/room_show',
+  //     success: function(res) {},
+  //     fail: function(res) {},
+  //     complete: function(res) {},
+  //   })
+  // },
   /**
    * Lifecycle function--Called when page load
    */
