6f005d7cb7ea3998f8f082dd06f27767401db402
munalombe <3223352545@qq.com>
2021-07-25 21:17:30 +0800

    added search feature

--- pages/rooms_index/rooms_index.js
+++ pages/rooms_index/rooms_index.js
@@ -12,19 +12,33 @@ Page({
   /**
    * Lifecycle function--Called when page load
    */
+  formSubmit:function(e){
+    console.log(e.detail.value.search)
+    this.onLoad(e.detail.value)
+  },
+
   onLoad: function (options) {
     const page = this;
     const url = app.globalData.url
-    // console.log("page loaded")
+    const search = options.search? `?query=${options.search}`: ""
+    // console.log("search",search)
+    // ?query=${options.search}
     wx.request({
-      url: `${url}/api/v1/rooms`,
+      url: `${url}/api/v1/rooms${search}`,
       method: 'GET',
       success(res) {
         // console.log(res)
         const rooms = res.data.rooms;
         // console.log("rooms:", rooms)
+        if(search != ""){
+          wx.showLoading({
+            title: 'searching',
+            duration: 2000
+          })
+        }else{}
         page.setData({
-          rooms: rooms
+          rooms: rooms,
+          value: ""
         });
         wx.hideToast();
       }
